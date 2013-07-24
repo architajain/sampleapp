be2d17dc25fbb3d960ccf291c62c5f64ac83159c
ruby -v <architajain.princy@gmail.com>
2013-07-24 18:54:43 +0530

    micropost

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,27 @@
+class MicropostsController < ApplicationController
+
+	before_filter :signed_in_user,only: [:create, :destroy]
+
+	def create
+		@micropost = current_user.microposts.build(params[:micropost])
+          if @micropost.save
+            flash[:success] = "Micropost created!"
+            redirect_to root_path
+          else
+          	@feed_items = []
+             render 'static_pages/home'
+           end
+	end
+
+	def destroy
+	end
+
+	def index
+		
+	end
+
+	def feed
+      microposts
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd019c047eeecae7e7523945e083602a54e428f5e404c0c541f350a82372cbf33acdf62971e951888d1d55f225f97f8a64f1f9f48fa2bc96be6e044c2b40e8906'
